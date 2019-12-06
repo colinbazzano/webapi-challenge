577bb0c15074649e680668271f0a302c69ab6d25
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2019-12-06 13:12:05 -0500

    Creating an action on a specified post is in working order

--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -19,6 +19,59 @@ router.get("/:id", validateProjectId, (req, res) => {
     });
 });
 
+// router.get("/", (req, res) => {
+//   res.status(200).json({ message: "hello" });
+// });
+
+// POST for projects.
+router.post("/", validateProject, (req, res) => {
+  const projectData = req.body;
+  project
+    .insert(projectData)
+    .then(project => {
+      res.status(201).json(project);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "Error creating the project." });
+    });
+});
+
+// PUT for projects.
+router.put("/:id", validateProjectId, validateProject, (req, res) => {
+  const projectData = req.body;
+  const id = req.params.id;
+
+  project
+    .update(id, projectData)
+    .then(project => {
+      project
+        ? res.status(200).json({ ...project, ...projectData })
+        : res.status(404).json({ message: "The project could not be found." });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "Error updating the project." });
+    });
+});
+
+// DELETE for projects.
+router.delete("/:id", validateProjectId, (req, res) => {
+  const id = req.params.id;
+  project
+    .remove(id)
+    .then(project => {
+      project > 0
+        ? res.status(200).json({ message: "The project has been removed!" })
+        : res.status(404).json({
+            message: "The project with the specified ID does not exist."
+          });
+    })
+    .catch(error => {
+      res.status(500).json({ message: "The project could not be removed." });
+    });
+});
+
 // GET for actions.
 router.get("/:id/actions", validateProjectId, (req, res) => {
   const id = req.params.id;
@@ -38,57 +91,37 @@ router.get("/:id/actions", validateProjectId, (req, res) => {
     });
 });
 
-// router.get("/", (req, res) => {
-//   res.status(200).json({ message: "hello" });
-// });
-
-router.post("/", validateProject, (req, res) => {
-  const projectData = req.body;
-  project
-    .insert(projectData)
-    .then(project => {
-      res.status(201).json(project);
-    })
-    .catch(error => {
-      console.log(error);
-      res.status(500).json({ message: "Error creating the project." });
-    });
-});
-
-router.put("/:id", validateProjectId, validateProject, (req, res) => {
-  const projectData = req.body;
+// POST for actions
+router.post("/:id/actions", (req, res) => {
   const id = req.params.id;
-
+  const projectData = req.body;
   project
-    .update(id, projectData)
+    .get(id)
     .then(project => {
       project
-        ? res.status(200).json({ ...project, ...projectData })
-        : res.status(404).json({ message: "The project could not be found." });
-    })
-    .catch(error => {
-      console.log(error);
-      res.status(500).json({ message: "Error updating the project." });
-    });
-});
-
-router.delete("/:id", validateProjectId, (req, res) => {
-  const id = req.params.id;
-  project
-    .remove(id)
-    .then(project => {
-      project > 0
-        ? res.status(200).json({ message: "The project has been removed!" })
+        ? action
+            .insert(projectData)
+            .then(action => {
+              res.status(201).json(action);
+            })
+            .catch(error => {
+              res.status(500).json({ message: "Could not create action." });
+            })
         : res
             .status(404)
             .json({
-              message: "The project with the specified ID does not exist."
+              message: "The project with the specified ID does not exist. "
             });
     })
     .catch(error => {
-      res.status(500).json({ message: "The project could not be removed." });
+      console.log(error);
+      res
+        .status(500)
+        .json({ message: "There was an error saving the action." });
     });
 });
+
+// CUSTOM MIDDLEWARE
 
 function validateProjectId(req, res, next) {
   const id = req.params.id;
@@ -114,5 +147,8 @@ function validateProject(req, res, next) {
     next();
   }
 }
+// function validateAction(req, res, next) {
+//     const actionData = req.body;
+// }
 
 module.exports = router;
