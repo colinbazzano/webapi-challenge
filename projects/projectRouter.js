f0d426cea20788d5cf5a8de22163a02e8c2e0657
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2019-12-06 12:18:25 -0500

    created custom middleware to validate that the project id does exist

--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -4,7 +4,7 @@ const action = require("../data/helpers/actionModel");
 
 const router = express.Router();
 
-router.get("/:id", (req, res) => {
+router.get("/:id", validateProjectId, (req, res) => {
   const projectData = req.params.id;
   console.log(projectData);
   project
@@ -27,5 +27,19 @@ router.post("/", (req, res) => {});
 router.put("/:id", (req, res) => {});
 
 router.delete("/:id", (req, res) => {});
+
+function validateProjectId(req, res, next) {
+  const id = req.params.id;
+  project
+    .get(id)
+    .then(id => {
+      id
+        ? next()
+        : res.status(400).json({ message: "Project ID does not exist." });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
 
 module.exports = router;
